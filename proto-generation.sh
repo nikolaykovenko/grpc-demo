8a2dc9c34535db0a7221f47ae9c47c5d3be4704b
Nickk <nickk@templatemonster.me>
2019-03-19 09:37:31 +0200

    Dockerized

--- proto-generation.sh
+++ proto-generation.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-protoc --proto_path=protos \
-  --php_out=generated \
-  --grpc_out=generated \
-  --plugin=protoc-gen-grpc=./plugins/grpc_php_plugin \
-  ./protos/drivers.proto
+docker run --rm -it -v $(pwd):/app -w /app grpc/php bash -c "protoc --php_out=generated --grpc_out=generated --plugin=protoc-gen-grpc=./plugins/grpc_php_plugin ./protos/drivers.proto"
